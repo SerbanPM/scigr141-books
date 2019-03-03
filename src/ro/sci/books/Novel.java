47a170e6cfd225d850226e02904191117c0e2006
SerbanPM2 <pmserban2@gmail.com>
2019-03-03 21:20:30 +0200

    Update Novel.java

--- src/ro/sci/books/Novel.java
+++ src/ro/sci/books/Novel.java
@@ -1,23 +1,64 @@
 package ro.sci.books;
 
+/**
+ * <h1>Novel</h1>
+ * Child class of the Book class.
+ * Used to create more specific instances of the Book class.
+ *
+ * @author Serban PM
+ * @version 1.0
+ * @see Book
+ * @since 20190122
+ */
+
+/**
+ * Represents a category of books that can be found in this library.
+ */
 public class Novel extends Book {
+
+    /**
+     * The type of the novel.
+     */
     private String type;
 
+    /**
+     * Default constructor.
+     */
     public Novel( ){
     }
 
+    /**
+     * Creates an instance of Novel with the specified type.
+     *
+     * @param type A string containing the type of this novel.
+     */
     public Novel(String type){
         this.type = type;
     }
 
+    /**
+     * Getter method which returns the type of this novel.
+     *
+     * @return A string representing the type of this novel.
+     */
     public String getType( ){
         return type;
     }
 
+    /**
+     * Setter method used to input the type of this novel.
+     *
+     * @param type A string containing the type of this novel.
+     */
     public void setType(String type){
         this.type = type;
     }
 
+    /**
+     * Method overriden from the {@link Book} class.
+     *
+     * @return a String containing a specific representation of ArtAlbum class instances.
+     */
     @Override
     public String toString( ){
         return "Novel: " + "nrPagini = " + getNrPagini() + " / nume = '" + getNume() + '\'' + " / gen literar = '" + type + '\'';
