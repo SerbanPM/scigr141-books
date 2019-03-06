6b160ac344270b6511000beeaf68348022f0170e
SerbanPM2 <pmserban2@gmail.com>
2019-03-06 22:24:50 +0200

    Create SetLibrary.java

---
+++ src/ro/sci/library/SetLibrary.java
@@ -0,0 +1,14 @@
+package ro.sci.library;
+
+public class SetLibrary {
+
+    @Override
+    public int hashCode( ){
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        return super.equals(obj);
+    }
+}
