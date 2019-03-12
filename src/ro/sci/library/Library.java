0e1b63dda92c6c184690c0ebc7b873265f98a977
SerbanPM2 <pmserban2@gmail.com>
2019-03-12 17:22:03 +0200

    Update Library.java

--- src/ro/sci/library/Library.java
+++ src/ro/sci/library/Library.java
@@ -7,7 +7,11 @@ import java.util.List;
 
 /**
  * <h1>Library</h1>
- * Implements adding, removing, classifying, centralizing books in the library.
+ *
+ * Implements {@link #addBook} method which is adding an object of type Book to the array list.
+ * Implements {@link #getItemCount} method which Counts the objects in the list using the size() method.
+ * Implements {@link #search} method which is searching for a title in the array list using the name of the object.
+ * Implements {@link #remove} method which is removing an object from the list.
  * Creates a text file containing all the instances added and not removed from library.
  * Permits searching by name or by position number in the book list.
  *
@@ -19,7 +23,7 @@ import java.util.List;
 /**
  * Contains the "business" part of this library.
  */
-public class Library {
+public class Library implements LibraryInterface {
 
     /**
      * Represents a list of books that can be found in this library.
@@ -31,7 +35,7 @@ public class Library {
      */
     private int numberOfItemsInLibrary;
 
-    public Library(Object books){
+    public Library(Book books){
 
     }
 
@@ -65,37 +69,37 @@ public class Library {
      *
      * @param carte An object of type Book.
      */
-    public void addBook(Book carte){
-        listOfBooks.add(carte);
+    public void addBook(Book book){
+        listOfBooks.add(book);
     }
 
     /**
      * Counts the objects in the list using the size() method.
      *
-     * @return itemCount An int representing the number of objects in the list.
+     * @return numberOfItemsInLibrary An int representing the number of objects in the list.
      */
     public int getItemCount( ){
         numberOfItemsInLibrary = listOfBooks.size();
         return numberOfItemsInLibrary;
     }
 
-    /**
+     /**
      * Searches for a title in the array list using the name of the object.
      * <p>It compares the name of the searched object with the names of the objects
      * from the list using the equals method. Prints a different message for both
      * cases, match or no match.</p>
      *
-     * @param ceCauti A string containing the title of the object to search.
+     * @param book A string containing the title of the object to search.
      * @return carte An object matching the searched title.
      */
-    public Book search(String ceCauti){
+    public Book search(String book){
         for (Book carte : listOfBooks) {
-            if (ceCauti.equals(carte.getNume())) {
-                System.out.println("Cartea cu titlul " + '\'' + ceCauti + '\'' + " exista in aceasta biblioteca!");
+            if (book.equals(carte.getNume())) {
+                System.out.println("Cartea cu titlul " + '\'' + book + '\'' + " exista in aceasta biblioteca!");
                 return carte;
             }
         }
-        System.out.println("Cartea cu titlul " + '\'' + ceCauti + '\'' + " nu exista in aceasta bilioteca!");
+        System.out.println("Cartea cu titlul " + '\'' + book + '\'' + " nu exista in aceasta bilioteca!");
         return null;
     }
 
@@ -103,7 +107,7 @@ public class Library {
      * Removes an object from the list.
      * The book is removed if the object is the same with the one from the method call.
      *
-     * @param book A string containing the name of the instance.
+     * @param book The instance to be removed.
      */
     public void remove(Book book){
         listOfBooks.remove(book);
